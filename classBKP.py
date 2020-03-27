30e4bc2f1fca55cf2e8f579f55c8526b7687bccc
Engineer3 <zinin.alesej2010@yandex.ru>
2020-03-27 16:47:24 +0300

    диплома

--- classBKP.py
+++ classBKP.py
@@ -1,6 +1,22 @@
+# >>> print('Omega: \u03A9')
+# Omega: Ω
+# >>> print('Delta: \u0394')
+# Delta: Δ
+# >>> print('sigma: \u03C3')
+# sigma: σ
+# >>> print('mu: \u03BC')
+# mu: μ
+# >>> print('epsilon: \u03B5')
+# epsilon: ε
+# >>> print('degree: \u00B0')
+# degree: °
+# >>> print('6i\u0302 + 4j\u0302-2k\u0302')
+# 6î + 4ĵ-2k
+import time
+import matplotlib.pyplot as plt
 import numpy
-s = 1.0
-
+from sympy import *
+var('s, t, z')
 
 class ControlDevise:
     """Регулятор положения ПП"""
@@ -13,34 +29,65 @@ class ControlDevise:
         self.t2 = t2
         self.tu = tu
         self.mu = mu
+        # self.z = z
+        # self.t = t
         # print("регулятор положения создан")
 
     def processing(self):
         """Обработка сигнала задания и сигнала с ДПП,
         выдача напряжения U на электронный усилитель"""
-        u = ((self. t2 * (s ** 2) + self.t1 * s + 1) * (self.t1 * s + 1))\
-            /((self.mu * s + 1) ** 2 * self.tu * s)
-        return u
+        fp = ((self.t2 * s ** 2 + self.t1 * s + 1) * (self.t1 * s + 1))\
+             / (((self.mu * s + 1) ** 2) * self.tu * s)
+
+        return fp
+    def podstanovka(self):
+        """одставнока оператора s в ПФ РП, для определения дискретной передаточной функции"""
+        s = (z - 1) / z * t
+        fp = ((self.t2 * s ** 2 + self.t1 * s + 1) * (self.t1 * s + 1)) \
+             / (((self.mu * s + 1) ** 2) * self.tu * s)
+
+        return fp
+
+class ElBooster:
+    """Создаем класс электронный усилитель"""
+
+    def __init__(self, fp, fps, ky):
+        """Инициализируем атрибуты: сигнал с регулятора ПП"""
+        self.fp = fp
+        self.fps = fps
+        self.ky = ky
+
+    def boosting(self):
+        """Обработка сигнала с регулятора положения ПП и выдача усиленного сигнала ..."""
+        uy = self.fps * self.ky
+
+        return uy
 
 
-regular = ControlDevise(uxz1=1.0, ux=1.0, t1=1.0, t2=1.0, tu=1.0, mu=1.0,)
-u = regular.processing()
-print(u)
 
-# class ElectronBooster:
-#     """"Электронный усилитель"""
+start = time.time
+regular = ControlDevise(uxz1=1, ux=2, t1=0.2, t2=0.125, tu=0.04, mu=0.001)
+fp = regular.processing()
+print(f'Передаточная функция имеет вид \n W(s) = {fp} \n')
+
+ht = inverse_laplace_transform(fp, s, t)
+
+stop = time.time()
+
+f = lambdify(t, ht, 'numpy')
+tt = numpy.arange(0.01, 20, 0.05)
+plt.title('Переходные характеристики регуляторов \n с передаточными функциями: \n ПИД - W(s)=%s \n ПИ - W(s)=%s' % (fp, fpp))
+plt.plot(tt, f(tt), color='r', linewidth=2, label='ПИД-регулятор: h(t)=%s' % ht)
+plt.grid(True)
+plt.legend(loc='best')
+plt.show()
+
+
+
+# regular1 = ControlDevise(uxz1=1, ux=2, t1=0.2, t2=0.125, tu=0.04, mu=0.001)
+# fps = regular1.podstanovka()
+# print(f'Дискретная передаточная функция имеет вид \n W(s) = {fps} \n')
 #
-#     def __init__(self, u, ky):
-#         """Инициализируем атрибуты: сигнал с регулятора положения ПП"""
-#         self.u = u
-#         self.ky = ky
-#         # print("электронный усилитель создан")
-#
-#     def boosting(self):
-#         """Обработка сигнала с регулятора положения ПП и выдача усиленного сигнала ..."""
-#         uy = self.u * self.ky
-#         return uy
-#
-# electrboost = ElectronBooster(u, 10)
+# electrboost = ElBooster(fp, fps, 10)
 # uy = electrboost.boosting()
-# print(uy)
+# print(f'Сигнал с эл. усилителя \n Uy = {uy} \n')
